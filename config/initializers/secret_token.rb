12b79b71d1885a48f9ea2ca6583f9b4df9a898a2
vovan <vovan@vovan-HP-620.(none)>
2012-08-16 22:29:19 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendar::Application.config.secret_token = 'a4f7ef184ad3a797ce8b9df73d5317f399b0129ac9dfbc49c1aa69e8ba65ca61ba3ef95861c41cd94562cfb67be8ad792e35b95a15c1ad5087b4947d03aab447'
