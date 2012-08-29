7763f0cb0622f9efd2085b7f180dca7c955ed7fe
vovan <vkweberti@gmail.com>
2012-08-29 08:03:32 +0300

    Настройка календаря

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,7 +6,9 @@ class CalendarController < ApplicationController
 
     @shown_month = Date.civil(@year, @month)
 
-    @event_strips = Event.event_strips_for_month(@shown_month)
+    @first_day_of_week = 1
+
+    @event_strips = Event.event_strips_for_month(@shown_month, @first_day_of_week)
   end
   
 end
